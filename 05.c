e0da610c7b496bddf908b8c739764d56bffdfcb1
Everson <143636906+Everson777@users.noreply.github.com>
2023-12-12 21:46:37 -0300

    Add files via upload

--- 05.c
+++ 05.c
@@ -1,53 +1,50 @@
 #include <stdio.h>
-#include <string.h>
 
-
-struct Funcionario {
+struct FUNc {
     char nome[50];
-    char cargo[50];
+    char cargo[30];
     float salario;
 };
 
-
-float calcularMediaSalarial(struct Funcionario funcionarios[], int numFuncionarios, const char cargoAlvo[]) {
-    float somaSalarial = 0;
+float calcularMediaSalarial(struct FUNc funcionarios[], int tamanho, const char cargoBusca[]) {
     int contador = 0;
+    float somaSalarios = 0;
 
-    for (int i = 0; i < numFuncionarios; i++) {
-        if (strcmp(funcionarios[i].cargo, cargoAlvo) == 0) {
-            somaSalarial += funcionarios[i].salario;
+    for (int i = 0; i < tamanho; ++i) {
+        if (strcmp(funcionarios[i].cargo, cargoBusca) == 0) {
+            somaSalarios += funcionarios[i].salario;
             contador++;
         }
     }
 
-    
     if (contador > 0) {
-        return somaSalarial / contador;
+        return somaSalarios / contador;
     } else {
         return 0; 
     }
 }
 
 int main() {
+    const int tamanhoFuncionarios = 5; 
+    struct FUNc funcionarios[tamanhoFuncionarios];
+
+    for (int i = 0; i < tamanhoFuncionarios; ++i) {
+        printf("Cadastro do Funcionário %d:\n", i + 1);
+        printf("Nome: ");
+        scanf("%s", funcionarios[i].nome);
+        printf("Cargo: ");
+        scanf("%s", funcionarios[i].cargo);
+        printf("Salário: ");
+        scanf("%f", &funcionarios[i].salario);
+        printf("\n");
+    }
+
     
-    struct Funcionario empresa[5] = {
-        {"Alice", "Programador", 5000.00},
-        {"Bob", "Analista de Sistemas", 4500.00},
-        {"Charlie", "Programador", 6000.00},
-        {"David", "Gerente de Projeto", 7000.00},
-        {"Eva", "Programador", 5500.00},
-    };
+    const char cargoProgramador[] = "Programador";
+    float mediaSalarialProgramadores = calcularMediaSalarial(funcionarios, tamanhoFuncionarios, cargoProgramador);
 
-    const char cargoAlvo[] = "Programador";
-
-    
-    int numFuncionarios = sizeof(empresa) / sizeof(empresa[0]);
-
-    
-    float mediaSalarial = calcularMediaSalarial(empresa, numFuncionarios, cargoAlvo);
-
-    if (mediaSalarial > 0) {
-        printf("A média salarial dos programadores na empresa é: R$%.2f\n", mediaSalarial);
+    if (mediaSalarialProgramadores > 0) {
+        printf("A média salarial dos programadores na empresa é: R$%.2f\n", mediaSalarialProgramadores);
     } else {
         printf("Não há programadores na empresa.\n");
     }
